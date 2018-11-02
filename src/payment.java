33a07de193880b1cc9231625f94e77b24f79d57d
bb <jassadajuntum@gmail.com>
2018-11-02 09:56:37 +0700

    make UI payment

--- src/payment.java
+++ src/payment.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class payment {
+    private JButton confirmButton;
+    private JPanel mainPanel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Payment");
+        payment form = new payment();
+        frame.setContentPane(form.mainPanel);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setMinimumSize(new Dimension(600, 250));
+        frame.setVisible(true);
+    }
 }
